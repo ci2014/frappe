69e7ccb2afbe648188b536e1d4017933c91d1f17
Ubuntu <ubuntu@ip-172-31-9-137.eu-central-1.compute.internal>
2017-11-20 00:00:23 +0100

    x

--- frappe/public/js/frappe/form/controls/data.js
+++ frappe/public/js/frappe/form/controls/data.js
@@ -13,7 +13,7 @@ frappe.ui.form.ControlData = frappe.ui.form.ControlInput.extend({
 		if (this.df.fieldname == 'name') {
 			$('<span class="input-group-addon"><i class="octicon octicon-search text-muted small"></i></span>').prependTo(this.input_area);
 		}
-		
+
 		if (in_list(['Data', 'Link', 'Dynamic Link', 'Password', 'Select', 'Read Only', 'Attach', 'Attach Image'],
 			this.df.fieldtype)) {
 			this.$input.attr("maxlength", this.df.length || 140);
